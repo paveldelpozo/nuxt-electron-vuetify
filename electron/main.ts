03ab79e733b1a97af3ed1387bd33daae47b0239d
paveldelpozo <pdelpozo@phicus.es>
2024-06-12 15:57:43 +0200

    fix loop on window created caused by bad nuxt new version. Downgraded to 3.11.0

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,5 @@
 import { app, BrowserWindow } from 'electron'
 
 app.whenReady().then(() => {
-    if (process.env.VITE_DEV_SERVER_URL) new BrowserWindow().loadURL(process.env.VITE_DEV_SERVER_URL)
+    new BrowserWindow().loadURL(process.env.VITE_DEV_SERVER_URL)    
 })
